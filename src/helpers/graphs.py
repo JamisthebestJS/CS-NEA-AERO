b51152f044b4821f84d5f96cee9c56a7e7add93b
JamisthebestJS <jamies172008@gmail.com>
2025-12-01 21:47:07 +0000

    V3.01 - final changes before swapping phys location
    Made a minor change after last push, but am swapping locations, so dont have access to this device, so am pushing again to save these changes.
    Added a case during the update graph which, every time 5k points are being plotted, halves the number of points to increase performance for large iteration counts (as otherwise plotting the graph becomes a bottleneck).
    Also fixed an issue where going back to main menu (not currently possible due to an issue with menus) would not restart the graph, so restarting the simulation would break the graphing feature

--- src/helpers/graphs.py
+++ src/helpers/graphs.py
@@ -20,6 +20,8 @@ def render_graph():
     if graph != None:
         del(graph)
     # plotting newer graph
+    
+
     plt.plot(iteration_list, lift_list, color='r', label='Lift')
     plt.plot(iteration_list, drag_list, color='g', label='Drag')
     plt.xlabel("Iteration")
@@ -32,10 +34,17 @@ def update_graphs(drag_item, lift_item, it_count):
     drag_list.append(drag_item)
     lift_list.append(lift_item)
     iteration_list.append(it_count)
-    
+    #every 5k iterations, halve the number of points to plot to ensure not too many points to plot at higher iteration levels
+    if len(iteration_list) % 5000 == 0:
+        for i in range(len(iteration_list)):
+            if i%2 == 0:
+                del(drag_list[i])
+                del(lift_list[i])
+                del(iteration_list[i])    
     render_graph()
 
 def clear_graph():
+    global drag_list, lift_list, iteration_list, graph
     drag_list = []
     lift_list = []
     iteration_list = []
